1e3f56b39a268770a7801cc4e226df15910f2d7a
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-08-08 14:33:34 +0400

    +

--- 06_08_2024/Services/ProductService.cs
+++ 06_08_2024/Services/ProductService.cs
@@ -9,18 +9,38 @@ namespace _06_08_2024.Services
         AppDbContext _context = new AppDbContext();
         public async Task CreateAsync(Product product)
         {
+            var isExist = await _context.Products.AnyAsync(x => x.Name.ToLower() == product.Name.ToLower());
+            if (isExist)
+            {
+                Console.WriteLine("Bu product artiq movcuddur");
+                return;
+            }
+            var isExistCategory = await _context.Categories.AnyAsync(x => x.Id == product.CategoryId);
+            if (!isExistCategory)
+            {
+                Console.WriteLine("Category tapilmadi");
+                return;
+            }
+            if (product.Price < 0)
+            {
+                Console.WriteLine("Price 0 dan kicik ola bilmez");
+                return;
+            }
+
+            product.Id = 0;
+
             await _context.AddAsync(product);
             await _context.SaveChangesAsync();
         }
         public async Task<List<Product>> GetAllAsync()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _context.Products.Include(x => x.Category).ToListAsync();
             return products;
         }
 
         public async Task<Product> GetByIdAsync(int id)
         {
-            var product = await _context.Products.FirstOrDefaultAsync(s => s.Id == id);
+            var product = await _context.Products.Include(x => x.Category).FirstOrDefaultAsync(s => s.Id == id);
             if (product == null)
             {
                 throw new Exception("Product not found.");
